d620278de0e6efeb5781c0632e1550c0f934d37a
Oscar Mier <voscarmv@gmail.com>
2025-05-27 11:30:21 -0500

    Self promotion

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="p-4 font-mono">
       <h1 className="text-xl mb-4">Hypno.ai Chat</h1>
+      <b>Author: </b>Oscar Mier voscarmv@gmail.com (<a href="https://linkedin.com/in/oscar-mier">Hire me!</a>)<br></br><br></br>
       <ChatInterface />
       <Analytics />
     </div>
