9c25615e8222bc1b0b8e55af9b3f6cb1834c66a5
Oscar Mier <voscarmv@gmail.com>
2025-05-26 23:35:54 -0500

    Analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { initializeThread } from './store/chatSlice';
 import ChatInterface from './components/ChatInterface';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ function App() {
     <div className="p-4 font-mono">
       <h1 className="text-xl mb-4">Hypno.ai Chat</h1>
       <ChatInterface />
+      <Analytics />
     </div>
   );
 }
