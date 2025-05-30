a611e6391ad9540d21e3195ab6a03465133b15b0
Oscar Mier <voscarmv@gmail.com>
2025-05-29 22:29:24 -0500

    Instructions 2

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
     <div className="p-4 font-mono">
       <h1 className="text-xl mb-4">Hypno.ai Chat</h1>
       <p><b>Author: </b>Oscar Mier <a href="mailto:voscarmv@gmail.com">voscarmv@gmail.com</a> (<a href="https://linkedin.com/in/oscar-mier">Hire me!</a>)</p>
-      <p>Instructions
+      <p>
         <ul>
-            <li><a href="https://youtube.com" target="_blank" rel="noreferrer">Español</a></li>
-            <li><a href="https://youtube.com" target="_blank" rel="noreferrer">English</a></li>
+            <li><a href="https://youtube.com" target="_blank" rel="noreferrer">Instricciones en Español</a></li>
+            <li><a href="https://youtube.com" target="_blank" rel="noreferrer">Instructions in English</a></li>
         </ul>
     </p>
     <ChatInterface />
